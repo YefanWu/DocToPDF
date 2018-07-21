55ca8f97ab8f52894c6b7c716734dae4a3543a97
Yefan Wu <yefan.wu@outlook.com>
2018-07-21 12:20:21 +0800

    Disabled form maximize

--- DocToPDF/Form1.Designer.cs
+++ DocToPDF/Form1.Designer.cs
@@ -134,6 +134,7 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Doc to PDF";
             this.Load += new System.EventHandler(this.Form1_Load);
